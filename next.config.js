7a09237aa6870c1465c39c232080d323c4e715ca
Radu Mojic <radu.mojic@elrond.com>
2023-07-17 14:47:47 +0300

    [+] updated @multiversx/sdk-dapp to 2.18.3 [+] updated branding: xPortal instead of Maiar, use https://tools.multiversx.com [+] updated config to avoid error logs generated by @walletconnect ( see more: https://github.com/WalletConnect/walletconnect-monorepo/issues/1908 )

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@multiversx/sdk-dapp']
+  transpilePackages: ['@multiversx/sdk-dapp'],
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  }
 };
 
 module.exports = nextConfig;
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const contractAddress =
-    'erd1qqqqqqqqqqqqqpgq72l6vl07fkn3alyfq753mcy4nakm0l72396qkcud5x';
+  'erd1qqqqqqqqqqqqqpgq72l6vl07fkn3alyfq753mcy4nakm0l72396qkcud5x';
 
 export const dAppName = 'NextJS Dapp';
 
@@ -8,7 +8,7 @@ export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
 
 export const apiTimeout = 6000;
 export const transactionSize = 15;
-export const TOOLS_API_URL = 'https://tools.elrond.com';
+export const TOOLS_API_URL = 'https://tools.multiversx.com';
 /**
  * Calls to these domains will use `nativeAuth` Baerer token
  */
