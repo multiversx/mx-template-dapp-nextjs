885c1879d0deaa52dee2d02ceb72db1f7c1d6eb6
Tudor Homoranu <tudor.homoranu@multiversx.com>
2023-10-10 13:18:52 +0300

    scripts updated

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,9 @@
 export default {
   e2e: {
     baseUrl: 'http://localhost:3000/',
-    defaultCommandTimeout: 30000,
-    responseTimeout: 70000,
-    requestTimeout: 70000,
+    defaultCommandTimeout: 15000,
+    responseTimeout: 15000,
+    requestTimeout: 15000,
     chromeWebSecurity: false,
     screenshotOnRunFailure: false,
     video: false,
