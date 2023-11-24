5cde3e7d0a2c0adeca205483014fded4bb675ab7
cipriandraghici <cipriandraghici93@gmail.com>
2023-11-24 18:36:46 +0200

    sdk-dapp, full ssr support

--- src/components/sdkDappComponents.ts
+++ src/components/sdkDappComponents.ts
@@ -1,134 +1,21 @@
 'use client';
-
-import dynamic from 'next/dynamic';
-
-export const TransactionsTable = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/TransactionsTable'))
-      .TransactionsTable;
-  },
-  { ssr: false }
-);
-
-export const Loader = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/Loader')).Loader;
-  },
-  { ssr: false }
-);
-
-export const PageState = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/PageState')).PageState;
-  },
-  { ssr: false }
-);
-
-export const SignTransactionsModals = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/SignTransactionsModals'))
-      .SignTransactionsModals;
-  },
-  { ssr: false }
-);
-export const NotificationModal = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/NotificationModal'))
-      .NotificationModal;
-  },
-  { ssr: false }
-);
-export const TransactionsToastList = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/TransactionsToastList'))
-      .TransactionsToastList;
-  },
-  { ssr: false }
-);
-
-export const ExtensionLoginButton = dynamic(
-  async () => {
-    return (
-      await import('@multiversx/sdk-dapp/UI/extension/ExtensionLoginButton')
-    ).ExtensionLoginButton;
-  },
-  { ssr: false }
-);
-
-export const OperaWalletLoginButton = dynamic(
-  async () => {
-    return (
-      await import(
-        '@multiversx/sdk-dapp/UI/operaWallet/OperaWalletLoginButton/OperaWalletLoginButton'
-      )
-    ).OperaWalletLoginButton;
-  },
-  { ssr: false }
-);
-
-export const WalletConnectLoginButton = dynamic(
-  async () => {
-    return (
-      await import(
-        '@multiversx/sdk-dapp/UI/walletConnect/WalletConnectLoginButton'
-      )
-    ).WalletConnectLoginButton;
-  },
-  { ssr: false }
-);
-
-export const LedgerLoginButton = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/ledger/LedgerLoginButton'))
-      .LedgerLoginButton;
-  },
-  { ssr: false }
-);
-
-export const WebWalletLoginButton = dynamic(
-  async () => {
-    return (
-      await import('@multiversx/sdk-dapp/UI/webWallet/WebWalletLoginButton')
-    ).WebWalletLoginButton;
-  },
-  { ssr: false }
-);
-
-export const FormatAmount = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/FormatAmount')).FormatAmount;
-  },
-  { ssr: false }
-);
-
-export const CopyButton = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/CopyButton')).CopyButton;
-  },
-  { ssr: false }
-);
-
-export const TransactionRow = dynamic(
-  async () => {
-    return (
-      await import(
-        '@multiversx/sdk-dapp/UI/TransactionsTable/components/TransactionRow'
-      )
-    ).TransactionRow;
-  },
-  { ssr: false }
-);
-
-export const ExplorerLink = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/UI/ExplorerLink')).ExplorerLink;
-  },
-  { ssr: false }
-);
-
-export const DappProvider = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/wrappers/DappProvider')).DappProvider;
-  },
-  { ssr: false }
-);
+export { CopyButton } from '@multiversx/sdk-dapp/UI/CopyButton/CopyButton';
+export { ExtensionLoginButton } from '@multiversx/sdk-dapp/UI/extension/ExtensionLoginButton/ExtensionLoginButton';
+export { FormatAmount } from '@multiversx/sdk-dapp/UI/FormatAmount/FormatAmount';
+export { LedgerLoginButton } from '@multiversx/sdk-dapp/UI/ledger/LedgerLoginButton/LedgerLoginButton';
+export { Loader } from '@multiversx/sdk-dapp/UI/Loader/Loader';
+export { NotificationModal } from '@multiversx/sdk-dapp/UI/NotificationModal/NotificationModal';
+export { OperaWalletLoginButton } from '@multiversx/sdk-dapp/UI/operaWallet/OperaWalletLoginButton/OperaWalletLoginButton';
+export { PageState } from '@multiversx/sdk-dapp/UI/PageState/PageState';
+export { SignTransactionsModals } from '@multiversx/sdk-dapp/UI/SignTransactionsModals/SignTransactionsModals';
+export { TransactionsTable } from '@multiversx/sdk-dapp/UI/TransactionsTable/TransactionsTable';
+export { WalletConnectLoginButton } from '@multiversx/sdk-dapp/UI/walletConnect/WalletConnectLoginButton/WalletConnectLoginButton';
+export { WebWalletLoginButton } from '@multiversx/sdk-dapp/UI/webWallet/WebWalletLoginButton/WebWalletLoginButton';
+export { XaliasLoginButton } from '@multiversx/sdk-dapp/UI/webWallet/XaliasLoginButton/XaliasLoginButton';
+export { AuthenticatedRoutesWrapper } from '@multiversx/sdk-dapp/wrappers/AuthenticatedRoutesWrapper/AuthenticatedRoutesWrapper';
+export { AxiosInterceptorContext } from '@multiversx/sdk-dapp/wrappers/AxiosInterceptorContext/AxiosInterceptorContext';
+export { DappProvider } from '@multiversx/sdk-dapp/wrappers/DappProvider/DappProvider';
+export { TransactionRow } from '@multiversx/sdk-dapp/UI/TransactionsTable/components/TransactionRow';
+export { ACCOUNTS_ENDPOINT } from '@multiversx/sdk-dapp/apiCalls/endpoints';
+export { ExplorerLink } from '@multiversx/sdk-dapp/UI/ExplorerLink';
+export { TransactionsToastList } from '@multiversx/sdk-dapp/UI/TransactionsToastList/TransactionsToastList';
