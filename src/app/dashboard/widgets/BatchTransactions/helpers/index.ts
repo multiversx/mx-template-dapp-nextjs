861be5f82db4efcd98a07674b4b334629a007447
Gavrila Andrei <andrei.gavrila@multiversx.com>
2023-12-28 11:01:05 +0200

    Update import

--- src/app/dashboard/widgets/BatchTransactions/helpers/index.ts
+++ src/app/dashboard/widgets/BatchTransactions/helpers/index.ts
@@ -1,5 +1,5 @@
 export * from './getBatchTransactions';
 export * from './getSwapAndLockTransactions';
-export * from './sendBatchTransactions';
+export * from './signWithoutSendingTransactions';
 export * from './signAndAutoSendBatchTransactions';
 export * from './swapAndLockTokens';
