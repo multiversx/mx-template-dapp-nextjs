b417e12c88c0772cd7d4e1281062d05b7a790ffd
razvan.tomegea <razvan.tomegea@gmail.com>
2024-11-04 16:08:38 +0200

    Updated sdks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,8 @@ import '../styles/globals.css';
 import { Suspense } from 'react';
 import type { ReactNode } from 'react';
 import { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { Layout } from '@/components/Layout';
 import App from './index';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Template dApp Next.js',
@@ -23,7 +20,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en' className={inter.className}>
+    <html lang='en'>
       <body>
         <App>
           <Suspense>
--- src/utils/sdkDappCore.ts
+++ src/utils/sdkDappCore.ts
@@ -1,6 +1,6 @@
 export { ContractFunction } from '@multiversx/sdk-core/out/smartcontracts/function';
 export { ResultsParser } from '@multiversx/sdk-core/out/smartcontracts/resultsParser';
-export { ProxyNetworkProvider } from '@multiversx/sdk-network-providers/out/proxyNetworkProvider';
+export { ProxyNetworkProvider } from '@multiversx/sdk-core/out/networkProviders/proxyNetworkProvider';
 export { Address } from '@multiversx/sdk-core/out/address';
 export { SmartContract } from '@multiversx/sdk-core/out/smartcontracts/smartContract';
 export { AbiRegistry } from '@multiversx/sdk-core/out/smartcontracts/typesystem/abiRegistry';
