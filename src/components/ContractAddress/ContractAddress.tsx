8a4e62bbfaff225786a0198fc60de83fa85eadd6
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-04-10 14:04:59 +0300

    Update contract address

--- src/components/ContractAddress/ContractAddress.tsx
+++ src/components/ContractAddress/ContractAddress.tsx
@@ -1,14 +1,16 @@
 import { contractAddress } from '@/config';
-import { MvxExplorerLink } from '@/lib';
+import { MvxExplorerLink, useGetNetworkConfig } from '@/lib';
 import { Label } from '@/components/Label';
 import { ACCOUNTS_ENDPOINT } from '@/localConstants';
 
 export const ContractAddress = () => {
+  const { network } = useGetNetworkConfig();
+
   return (
     <p>
       <Label>Contract: </Label>
       <MvxExplorerLink
-        link={`/${ACCOUNTS_ENDPOINT}/${contractAddress}`}
+        link={`${network.explorerAddress}/${ACCOUNTS_ENDPOINT}/${contractAddress}`}
         class='border-b border-dotted border-gray-500 hover:border-solid hover:border-gray-800'
         text={contractAddress}
       />
