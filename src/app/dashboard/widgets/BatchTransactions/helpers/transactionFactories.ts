91537cfe36a6ab16ead6406f6393299730bd165f
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-05-21 16:12:47 +0300

    Unify

--- src/app/dashboard/widgets/BatchTransactions/helpers/transactionFactories.ts
+++ src/app/dashboard/widgets/BatchTransactions/helpers/transactionFactories.ts
@@ -1,7 +1,39 @@
-import { BATCH_TRANSACTIONS_SC } from '@/config';
-import { Address, GAS_PRICE, Transaction, VERSION } from '@/lib';
-import { TransactionProps } from '@/types/transaction.types';
+import {
+  Address,
+  Transaction,
+  TransactionsFactoryConfig,
+  TransferTransactionsFactory,
+  GAS_PRICE,
+  VERSION
+} from '@/lib';
+import { TransactionProps } from '@/types';
 import BigNumber from 'bignumber.js';
+
+import { BATCH_TRANSACTIONS_SC } from '@/config';
+
+const NUMBER_OF_TRANSACTIONS = 5;
+
+export const getBatchTransactions = ({
+  address,
+  chainID
+}: TransactionProps): Transaction[] => {
+  const transactions = Array.from(Array(NUMBER_OF_TRANSACTIONS).keys());
+
+  const factoryConfig = new TransactionsFactoryConfig({ chainID });
+  const factory = new TransferTransactionsFactory({ config: factoryConfig });
+
+  return transactions.map((id) => {
+    const tokenTransfer = factory.createTransactionForNativeTokenTransfer(
+      Address.newFromBech32(address),
+      {
+        receiver: Address.newFromBech32(address),
+        nativeAmount: BigInt(new BigNumber(id).plus(1).shiftedBy(18).toFixed())
+      }
+    );
+
+    return tokenTransfer;
+  });
+};
 
 export const getSwapAndLockTransactions = ({
   address,
