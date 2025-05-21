2ca85daf2c945dc3efcdecfbaa0e3d3e9e813e6c
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-05-21 14:53:47 +0300

    Update service

--- src/app/dashboard/widgets/PingPongService/hooks/tests/useGetTimeToPong.test.ts
+++ src/app/dashboard/widgets/PingPongService/hooks/tests/useGetTimeToPong.test.ts
@@ -16,6 +16,10 @@ jest.mock('@/config', () => ({
   }
 }));
 
+jest.mock('@/config', () => ({
+  API_URL: 'https://devnet-template-api.multiversx.com'
+}));
+
 jest.mock(
   '@multiversx/sdk-dapp/out/store/selectors/hooks/network/useGetNetworkConfig',
   () => ({
@@ -25,11 +29,12 @@ jest.mock(
   })
 );
 
-describe('useGetTimeToPong', () => {
+describe('useGetTimeToPong - Service', () => {
   beforeEach(() => {
     (useGetLoginInfo as jest.Mock).mockReturnValue({
       tokenLogin: {
-        nativeAuthToken: 'mock-token'
+        nativeAuthToken:
+          'ZXJkMWhmczk5Mmh0eDBhaHFrYTI3dmZwa3dqcmZ5dWZ4NmZqNzlodWxnOHY1aG1tc2djZmNuOHN3MG54N3Q.YUhSMGNITTZMeTkxZEdsc2N5NXRkV3gwYVhabGNuTjRMbU52YlEuMzliMTA4YzM1YTBiMmRkNmRkNmI1NGJhNzc0NzExZDRmOTUyMWRkZjdhN2Q5MjA0MmNmNjEyNDE0YzdjYzM3ZC43MjAwLmV5SjBhVzFsYzNSaGJYQWlPakUzTkRjNE1qYzFORFo5.c50f3784a3d532e469b441c90a71747fabc9d7302ef8858aa5ed97a834344402abe2adbd19d44b0469941f4b10a27227cf52bec66fd54e8941aca4f8dc7fee0d'
       }
     });
   });
@@ -37,7 +42,6 @@ describe('useGetTimeToPong', () => {
   it('should return 180 seconds', async () => {
     jest.spyOn(axios, 'get').mockResolvedValueOnce({
       data: {
-        status: 'awaiting_pong',
         timeToPong: 180
       }
     });
