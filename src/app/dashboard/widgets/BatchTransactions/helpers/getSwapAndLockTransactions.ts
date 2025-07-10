893891a501a56eac625daf12b965502cca3575f9
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-07-10 13:12:39 +0300

    Added Unlock and Logout pages (#80)
    * Add unlock page
    
    * Add logout and unlock pages
    
    * Revert changes
    
    * Update transaction
    
    * Fix time
    
    * Update transactions
    
    * Update imports
    
    * Revert
    
    * Update token
    
    * Update handler

--- src/app/dashboard/widgets/BatchTransactions/helpers/getSwapAndLockTransactions.ts
+++ src/app/dashboard/widgets/BatchTransactions/helpers/getSwapAndLockTransactions.ts
@@ -1,39 +1,6 @@
-import {
-  Address,
-  Transaction,
-  TransactionsFactoryConfig,
-  TransferTransactionsFactory,
-  GAS_PRICE,
-  VERSION
-} from '@/lib';
-import { TransactionProps } from '@/types';
-import BigNumber from 'bignumber.js';
-
 import { BATCH_TRANSACTIONS_SC } from '@/config';
-
-const NUMBER_OF_TRANSACTIONS = 5;
-
-export const getBatchTransactions = ({
-  address,
-  chainID
-}: TransactionProps): Transaction[] => {
-  const transactions = Array.from(Array(NUMBER_OF_TRANSACTIONS).keys());
-
-  const factoryConfig = new TransactionsFactoryConfig({ chainID });
-  const factory = new TransferTransactionsFactory({ config: factoryConfig });
-
-  return transactions.map((id) => {
-    const tokenTransfer = factory.createTransactionForNativeTokenTransfer(
-      Address.newFromBech32(address),
-      {
-        receiver: Address.newFromBech32(address),
-        nativeAmount: BigInt(new BigNumber(id).plus(1).shiftedBy(18).toFixed())
-      }
-    );
-
-    return tokenTransfer;
-  });
-};
+import { Address, GAS_PRICE, Transaction, VERSION } from '@/lib';
+import { TransactionProps } from '@/types';
 
 export const getSwapAndLockTransactions = ({
   address,
@@ -50,7 +17,7 @@ export const getSwapAndLockTransactions = ({
         BATCH_TRANSACTIONS_SC.egld_wEGLD.contract
       ),
       sender: Address.newFromBech32(address),
-      value: BigInt(new BigNumber(1).shiftedBy(18).toFixed()),
+      value: BigInt('1000000000000000000'),
       version: VERSION,
       data: Uint8Array.from(Buffer.from(BATCH_TRANSACTIONS_SC.egld_wEGLD.data))
     }),
--- src/app/dashboard/widgets/PingPongService/hooks/useGetTimeToPong.ts
+++ src/app/dashboard/widgets/PingPongService/hooks/useGetTimeToPong.ts
@@ -1,24 +1,14 @@
 import axios from 'axios';
 import { API_URL } from '@/config';
 import { TimeToPongResponseType } from '../types';
-import { useGetLoginInfo } from '@/lib';
 
 export const useGetTimeToPong = () => {
-  const loginInfo = useGetLoginInfo();
-
   const getTimeToPong = async () => {
-    if (!loginInfo.tokenLogin?.nativeAuthToken) {
-      return;
-    }
-
     try {
       const { data } = await axios.get<TimeToPongResponseType>(
         '/ping-pong/abi/time-to-pong',
         {
-          baseURL: API_URL,
-          headers: {
-            Authorization: `Bearer ${loginInfo.tokenLogin.nativeAuthToken}`
-          }
+          baseURL: API_URL
         }
       );
 
