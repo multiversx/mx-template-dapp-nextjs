a154e1daf1f3d7f53d4c67f88007b3b3340f9f2a
Iulia Cimpeanu <iuliaalexandra.ignat@gmail.com>
2025-07-18 12:44:59 +0300

    Fixed build failing

--- src/lib/sdkDappUI/sdkDappUI.types.ts
+++ src/lib/sdkDappUI/sdkDappUI.types.ts
@@ -1,4 +1,4 @@
-export type { TransactionRowType } from '@multiversx/sdk-dapp-ui';
+export type { TransactionRowType } from '@multiversx/sdk-dapp-ui/types/components/controlled/transactions-table/transactions-table.type';
 export type { MvxCopyButton as MvxCopyButtonPropsType } from '@multiversx/sdk-dapp-ui/web-components/mvx-copy-button';
 export type { MvxExplorerLink as MvxExplorerLinkPropsType } from '@multiversx/sdk-dapp-ui/web-components/mvx-explorer-link';
 export type { MvxFormatAmount as MvxFormatAmountPropsType } from '@multiversx/sdk-dapp-ui/web-components/mvx-format-amount';
