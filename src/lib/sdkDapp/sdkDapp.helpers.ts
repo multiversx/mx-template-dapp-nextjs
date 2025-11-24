2013473f43338485a416146c5a0b7aad701e6566
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-11-24 17:33:50 +0200

    Update code

--- src/lib/sdkDapp/sdkDapp.helpers.ts
+++ src/lib/sdkDapp/sdkDapp.helpers.ts
@@ -3,7 +3,6 @@ export { NotificationsFeedManager } from '@multiversx/sdk-dapp/out/managers/Noti
 export { ProviderFactory } from '@multiversx/sdk-dapp/out/providers/ProviderFactory';
 export { TransactionManager } from '@multiversx/sdk-dapp/out/managers/TransactionManager';
 export { TransactionsTableController } from '@multiversx/sdk-dapp/out/controllers/TransactionsTableController';
-export { WALLET_PROVIDER_SEND_TRANSACTION_URL } from '@multiversx/sdk-dapp/out/constants/webWalletProvider.constants';
 export { getAccountProvider } from '@multiversx/sdk-dapp/out/providers/helpers/accountProvider';
 export { getActiveTransactionsStatus } from '@multiversx/sdk-dapp/out/utils/transactions/getActiveTransactionsStatus';
 export { getInterpretedTransaction } from '@multiversx/sdk-dapp/out/utils/transactions/getInterpretedTransaction';
